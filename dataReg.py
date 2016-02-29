8a5c408ec5756bbe3dd41661525654c001f94f63
MinyiX <testsxu00one@gmail.com>
2016-02-28 22:56:07 -0800

    added two modes of json dump: one is in json format, and the other is in string stream.

--- dataReg.py
+++ dataReg.py
@@ -1,18 +1,22 @@
-
 import re, json
+
+mode = "json_format"
 
 jsonFile = open('sample1.json', 'r')
 jsonStr = jsonFile.read()
 print jsonStr
 jsonData = json.loads(jsonStr)
 print json.dumps(jsonData, indent=4, sort_keys=True)
+
 fhOrig = open('unprocessed_data.json', 'w')
-#fhOrig.write(json.dumps(jsonData, indent=4, sort_keys=True))
-fhOrig.write(json.dumps(jsonStr, indent=4, sort_keys=True))
+if mode == "json_format":
+    fhOrig.write(json.dumps(jsonData, indent=4, sort_keys=True))
+else:
+    fhOrig.write(json.dumps(jsonStr, indent=4, sort_keys=True))
 fhOrig.close()
 
-listElement = ['start', 'duration', 'fwd_flow_id', 'rev_flow_id', 'id', \
-               'request_ts', 'response_ts', 'sensor', 'seq', 'response_time']
+listElement = ['start',      'duration',    'fwd_flow_id', 'rev_flow_id', 'id',
+               'request_ts', 'response_ts', 'sensor',      'seq',         'response_time']
 lelem = '|'.join(listElement)
 pat = re.compile(r'("('+lelem+r')":\s+[^,]+?(,\s*| |\}))')
 
@@ -25,8 +29,10 @@ print jsonProcessedStr
 
 print json.dumps(jsonProcessedData, indent=4, sort_keys=True)
 fhProc = open('processed_data.json', 'w')
-#fhProc.write(json.dumps(jsonProcessedData, indent=4, sort_keys=True))
-fhProc.write(json.dumps(jsonProcessedStr, indent=4, sort_keys=True))
+if mode == "json_format":
+    fhProc.write(json.dumps(jsonProcessedData, indent=4, sort_keys=True))
+else:
+    fhProc.write(json.dumps(jsonProcessedStr, indent=4, sort_keys=True))
 fhProc.close()
 
 
